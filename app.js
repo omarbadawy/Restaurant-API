808d735cdf3c9cf2393096c99cf85877d9ef4cb7
omarbadawy <omarbadawy10022@gmail.com>
2021-08-25 18:14:16 +0200

    Testing On production num 2 :'D

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 })
 
 const limiter = rateLimit({
-    max: 100,
+    max: 3,
     windowMs: 60 * 60 * 1000,
     message: 'Too many requests from this IP, please try again in an hour!',
 })
