14a98a5f7157a621c514f702557e3ab80132e3ec
omarbadawy <omarbadawy10022@gmail.com>
2021-08-27 13:19:17 +0200

    Changing rate limit temporarly

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 })
 
 const limiter = rateLimit({
-    max: 120,
+    max: 1000,
     windowMs: 60 * 60 * 1000,
     message: 'Too many requests from this IP, please try again in an hour!',
 })
